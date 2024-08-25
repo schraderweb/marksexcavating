6e5543519219e0401f5eb2bb7edc1c256cabec04
Med <med.houssni@gmail.com>
2024-08-25 17:17:26 +0100

    add nojkl file w

--- js/main.js
+++ js/main.js
@@ -118,3 +118,20 @@
     
 })(jQuery);
 
+
+$(document).ready(function() {
+
+
+	/* Apply fancybox to multiple items */
+	
+	$("a.group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false,
+        'showNavArrows' :   true
+	});
+	
+});
+
